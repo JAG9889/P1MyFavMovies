ede120bd0b348d5159a6bf9181683c6a290e3dea
Jordan <jordan.garner9889@gmail.com>
2015-11-16 14:21:12 -0700

    Update media.py

--- media.py
+++ media.py
@@ -1,5 +1,6 @@
 import webbrowser
 
+#This is the class movie which had had a few new added attributes.
 class Movie () : 
     def __init__(self, movie_title, movie_synopsis, movie_genre, movie_length, poster_image_url, trailer_youtube_url) : 
                  self.title = movie_title
@@ -9,5 +10,6 @@ class Movie () :
                  self.poster_image_url = poster_image_url
                  self.trailer_youtube_url = trailer_youtube_url
 
+#this function is used to open and display the movie trailer window. 
     def show_trailer(self):
         webbrowser.open(self.trailer_url)
