cc3bfe7ce658bbfd01a6d9b1407f9726ab9279f8
Jordan <jordan.garner9889@gmail.com>
2015-11-16 13:29:49 -0700

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,112 +4,112 @@ import media
 
 #instances of the Movie class
 midnight_in_paris = media.Movie("Midnight in Paris",
-                                                      "While on a trip to Paris with his fiancée's family, a nostalgic screenwriter finds himself mysteriously going back to the 1920s every day at midnight.",
-                                                      "Comedy, Fantasy, Romance",
-                                                      "94 mins",
-                                                      "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
-                                                      "https://youtu.be/BYRWfS2s2v4")
+                                "While on a trip to Paris with his fiancée's family, a nostalgic screenwriter finds himself mysteriously going back to the 1920s every day at midnight.",
+                                "Comedy, Fantasy, Romance",
+                                "94 mins",
+                                "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                                "https://youtu.be/BYRWfS2s2v4")
 
 a_good_year = media.Movie("A Good Year",
-                                               "A British investment broker inherits his uncle's chateau and vineyard in Provence, where he spent much of his childhood. He discovers a new laid-back lifestyle as he tries to renovate the estate to be sold.",
-                                               "Comedy, Drama, Romance",
-                                               "117 mins",
-                                               "https://reelreviewerblog.files.wordpress.com/2013/02/a-good-year-movie-poster.jpg",
-                                               "https://youtu.be/bKKrLj5ZHiM")
+                          "A British investment broker inherits his uncle's chateau and vineyard in Provence, where he spent much of his childhood. He discovers a new laid-back lifestyle as he tries to renovate the estate to be sold.",
+                          "Comedy, Drama, Romance",
+                          "117 mins",
+                          "https://reelreviewerblog.files.wordpress.com/2013/02/a-good-year-movie-poster.jpg",
+                          "https://youtu.be/bKKrLj5ZHiM")
 
 the_holiday = media.Movie("The Holiday",
-                                             "Two women troubled with guy-problems swap homes in each other's countries, where they each meet a local guy and fall in love.",
-                                             "Comedy, Romance",
-                                             "138 mins",
-                                             "http://ia.media-imdb.com/images/M/MV5BMTM2MzEyNDkxNF5BMl5BanBnXkFtZTcwODE5NjkzMQ@@._V1_SX640_SY720_.jpg",
-                                             "https://youtu.be/G0p8Su3bdHc")
+                          "Two women troubled with guy-problems swap homes in each other's countries, where they each meet a local guy and fall in love.",
+                          "Comedy, Romance",
+                          "138 mins",
+                          "http://ia.media-imdb.com/images/M/MV5BMTM2MzEyNDkxNF5BMl5BanBnXkFtZTcwODE5NjkzMQ@@._V1_SX640_SY720_.jpg",
+                          "https://youtu.be/G0p8Su3bdHc")
 
 the_parent_trap = media.Movie("The Parent Trap",
-                                                    "Identical twins, separated at birth and each raised by one of their biological parents, discover each other for the first time at summer camp and make a plan to bring their wayward parents back together.",
-                                                    "Adventure, Comedy, Drama",
-                                                    "128 mins",
-                                                    "http://movies.geourdu.com/wp-content/uploads/2015/06/The-Parent-Trap-1998.jpg",
-                                                    "https://youtu.be/32WeiH4TrIY")
+                              "Identical twins, separated at birth and each raised by one of their biological parents, discover each other for the first time at summer camp and make a plan to bring their wayward parents back together.",
+                              "Adventure, Comedy, Drama",
+                              "128 mins",
+                              "http://movies.geourdu.com/wp-content/uploads/2015/06/The-Parent-Trap-1998.jpg",
+                              "https://youtu.be/32WeiH4TrIY")
 
 ever_after = media.Movie("Ever After",
-                                           "The 'real' story of Cinderella. A refreshing new take on the classic fairy tale.",
-                                           "Comedy, Drama, Romance",
-                                           "121 mins",
-                                           "https://upload.wikimedia.org/wikipedia/en/6/6e/Everafterposter.jpg",
-                                           "https://youtu.be/Hcj9fyx6DXI")
+                         "The 'real' story of Cinderella. A refreshing new take on the classic fairy tale.",
+                         "Comedy, Drama, Romance",
+                         "121 mins",
+                         "https://upload.wikimedia.org/wikipedia/en/6/6e/Everafterposter.jpg",
+                         "https://youtu.be/Hcj9fyx6DXI")
 
 the_family_stone = media.Movie("The Family Stone",
-                                                      "An uptight, conservative, businesswoman accompanies her boyfriend to his eccentric and outgoing family's annual Christmas celebration and finds that she's a fish out of water in their free-spirited way of life.",
-                                                      "Comedy, Drama, Romance",
-                                                      "103 mins",
-                                                      "http://movieboozer.com/wp-content/uploads/2013/12/the-family-stone-original.jpg",
-                                                      "https://youtu.be/qHkwPz2j2IE")
+                               "An uptight, conservative, businesswoman accompanies her boyfriend to his eccentric and outgoing family's annual Christmas celebration and finds that she's a fish out of water in their free-spirited way of life.",
+                               "Comedy, Drama, Romance",
+                               "103 mins",
+                               "http://movieboozer.com/wp-content/uploads/2013/12/the-family-stone-original.jpg",
+                               "https://youtu.be/qHkwPz2j2IE")
 
 mrs_doubtfire = media.Movie("Mrs. Doubtfire",
-                                                 "After a bitter divorce, an actor disguises himself as a female housekeeper to spend time with his children held in custody by his former wife.",
-                                                 "Comedy, Drama, Family",
-                                                 "125 mins",
-                                                 "http://assets.flicks.co.nz/images/movies/poster/19/1963bd5135521d623f6c29e6b1174975_500x735.jpg",
-                                                 "https://youtu.be/DoAEouJTdWY")
+                            "After a bitter divorce, an actor disguises himself as a female housekeeper to spend time with his children held in custody by his former wife.",
+                            "Comedy, Drama, Family",
+                            "125 mins",
+                            "http://assets.flicks.co.nz/images/movies/poster/19/1963bd5135521d623f6c29e6b1174975_500x735.jpg",
+                            "https://youtu.be/DoAEouJTdWY")
 
 the_first_wives_club = media.Movie("The First Wives Club",
-                                                           "Reunited by the death of a college friend, three divorced women seek revenge on the husbands who left them for younger women.",
-                                                           "Comedy",
-                                                           "103 mins",
-                                                           "http://ecx.images-amazon.com/images/I/7172H9gv54S._SL1427_.jpg",
-                                                           "https://youtu.be/AI5OgeZt020")
+                                   "Reunited by the death of a college friend, three divorced women seek revenge on the husbands who left them for younger women.",
+                                   "Comedy",
+                                   "103 mins",
+                                   "http://ecx.images-amazon.com/images/I/7172H9gv54S._SL1427_.jpg",
+                                   "https://youtu.be/AI5OgeZt020")
 
 notting_hill = media.Movie("Notting Hill",
-                                            "The life of a simple bookshop owner changes when he meets the most famous film star in the world.",
-                                            "Comedy, Drama, Romance",
-                                            "124 mins",
-                                            "http://greengrapesandcoffee.blog.com/files/2014/10/Notting-Hill.jpg",
-                                            "https://youtu.be/4RI0QvaGoiI")
+                           "The life of a simple bookshop owner changes when he meets the most famous film star in the world.",
+                           "Comedy, Drama, Romance",
+                           "124 mins",
+                           "http://greengrapesandcoffee.blog.com/files/2014/10/Notting-Hill.jpg",
+                           "https://youtu.be/4RI0QvaGoiI")
 
 under_the_tuscan_sun = media.Movie ("Under the Tuscan Sun",
-                                                               "While on a trip to Paris with his fiancée's family, a nostalgic screenwriter finds himself mysteriously going back to the 1920s every day at midnight.",
-                                                               "Comedy, Drama, Romance",
-                                                               "113 mins",
-                                                               "http://images.killermovies.com/u/underthetuscansun/gallery/0_poster_under_the_tuscan_sun.jpg",
-                                                               "https://youtu.be/vdJGMZDY0-8")
+                                    "While on a trip to Paris with his fiancée's family, a nostalgic screenwriter finds himself mysteriously going back to the 1920s every day at midnight.",
+                                    "Comedy, Drama, Romance",
+                                    "113 mins",
+                                    "http://images.killermovies.com/u/underthetuscansun/gallery/0_poster_under_the_tuscan_sun.jpg",
+                                    "https://youtu.be/vdJGMZDY0-8")
 
 the_devil_wears_prada = media.Movie("The Devil Wears Prada",
-                                                                "A smart but sensible new graduate lands a job as an assistant to Miranda Priestly, the demanding editor-in-chief of a high fashion magazine.",
-                                                                "Comedy, Drama, Romance",
-                                                                "109 mins",
-                                                                "http://www.impawards.com/2006/posters/devil_wears_prada.jpg",
-                                                                "https://youtu.be/XTDSwAxlNhc")
+                                    "A smart but sensible new graduate lands a job as an assistant to Miranda Priestly, the demanding editor-in-chief of a high fashion magazine.",
+                                    "Comedy, Drama, Romance",
+                                    "109 mins",
+                                    "http://www.impawards.com/2006/posters/devil_wears_prada.jpg",
+                                    "https://youtu.be/XTDSwAxlNhc")
 
 love_actually = media.Movie("Love Actually",
-                                                "Follows the lives of eight very different couples in dealing with their love lives in various loosely interrelated tales all set during a frantic month before Christmas in London, England.",
-                                                "Comedy, Drama, Romance",
-                                                "135 mins",
-                                                "http://ecx.images-amazon.com/images/I/51-CU01Fp4L._SX940_.jpg",
-                                                "https://youtu.be/cYCkFTyADJ0")
+                            "Follows the lives of eight very different couples in dealing with their love lives in various loosely interrelated tales all set during a frantic month before Christmas in London, England.",
+                            "Comedy, Drama, Romance",
+                            "135 mins",
+                            "http://ecx.images-amazon.com/images/I/51-CU01Fp4L._SX940_.jpg",
+                            "https://youtu.be/cYCkFTyADJ0")
 
 practical_magic = media.Movie("Practical Magic",
-                                                    "The wry, comic romantic tale follows the Owens sisters, Sally and Gillian, as they struggle to use their hereditary gift for practical magic to overcome the obstacles in discovering true love.",
-                                                    "Comedy, Fantasy, Drama",
-                                                    "104 mins",
-                                                    "http://filmonizirani.net/wp-content/uploads/2015/06/practical-magic-poster.jpg",
-                                                    "https://youtu.be/go8bdi-EJQk")
+                              "The wry, comic romantic tale follows the Owens sisters, Sally and Gillian, as they struggle to use their hereditary gift for practical magic to overcome the obstacles in discovering true love.",
+                              "Comedy, Fantasy, Drama",
+                              "104 mins",
+                              "http://filmonizirani.net/wp-content/uploads/2015/06/practical-magic-poster.jpg",
+                              "https://youtu.be/go8bdi-EJQk")
 
 harry_potter_hbp = media.Movie("Harry Potter and the Half-Blood Prince",
-                                                      "As Harry Potter begins his sixth year at Hogwarts, he discovers an old book marked as 'the property of the Half-Blood Prince' and begins to learn more about Lord Voldemort's dark past.",
-                                                      "Adventure, Fantasy, Family",
-                                                      "153 mins",
-                                                      "http://thesmith.org/wp-content/uploads/2015/09/hp-and-the-Half-Blood-Prince-movie-poster.jpg",
-                                                      "https://youtu.be/4ukhVzqnUdI")
+                               "As Harry Potter begins his sixth year at Hogwarts, he discovers an old book marked as 'the property of the Half-Blood Prince' and begins to learn more about Lord Voldemort's dark past.",
+                               "Adventure, Fantasy, Family",
+                               "153 mins",
+                               "http://thesmith.org/wp-content/uploads/2015/09/hp-and-the-Half-Blood-Prince-movie-poster.jpg",
+                               "https://youtu.be/4ukhVzqnUdI")
 
 the_bourne_identity = media.Movie("The Bourne Identity",
-                                                          "A man is picked up by a fishing boat, bullet-riddled and suffering from amnesia, before racing to elude assassins and regain his memory.",
-                                                          "Action, Mystery, Thriller",
-                                                          "119 mins",
-                                                          "http://static.flickr.com/39/100950547_b9bab1a800_o.jpg",
-                                                          "https://youtu.be/FpKaB5dvQ4g")
+                                  "A man is picked up by a fishing boat, bullet-riddled and suffering from amnesia, before racing to elude assassins and regain his memory.",
+                                  "Action, Mystery, Thriller",
+                                  "119 mins",
+                                  "http://static.flickr.com/39/100950547_b9bab1a800_o.jpg",
+                                  "https://youtu.be/FpKaB5dvQ4g")
 
 #this creates an array of the Movie instances 
 movie = [midnight_in_paris, a_good_year, the_holiday, the_parent_trap, ever_after, the_family_stone, mrs_doubtfire,
                the_first_wives_club, notting_hill, under_the_tuscan_sun, the_devil_wears_prada, love_actually, practical_magic, harry_potter_hbp, the_bourne_identity]
-#this function calls on the array movie to generate the content 
+#this function calls on the movie array to generate the content 
 fresh_tomatoes.open_movies_page(movie)
